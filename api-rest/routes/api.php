d7831d738dee39788d1740d59ffa7fe417d3b228
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-12 22:14:17 -0500

    Invocando al Controlador UserController el cual contiene todas las funciones del CRUD para cada ruta especificada

--- api-rest/routes/api.php
+++ api-rest/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,36 +24,16 @@ Route::get('/', function(){
 });
 
 // Listar registros
-Route::get('users', function(){
-    return response()->json([
-        'message' => 'Listado de Usuarios'
-    ]);
-});
+Route::get('users', [UserController::class, 'index']);
 
 // Crear registros
-Route::post('users', function(){
-    return response()->json([
-        'message' => 'Usuario creado'
-    ]);
-});
+Route::post('users', [UserController::class, 'store']);
 
 // Recuperar registros
-Route::get('users/{id}', function($id){
-    return response()->json([
-        'message' => 'Usuario recuperado: ' . $id
-    ]);
-});
+Route::get('users/{id}', [UserController::class, 'show']);
 
 // Actualizar registros
-Route::put('users/{id}', function($id){
-    return response()->json([
-        'message' => 'Usuario actualizado: ' . $id
-    ]);
-});
+Route::put('users/{id}', [UserController::class, 'update']);
 
 // Eliminar registros
-Route::delete('users/{id}', function($id){
-    return response()->json([
-        'message' => 'Usuario eliminado: ' . $id
-    ]);
-});
+Route::delete('users/{id}', [UserController::class, 'destroy']);
