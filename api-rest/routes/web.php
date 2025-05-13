513fc04598b5d7d9b185e29d6792cc78d3589e10
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-12 22:53:46 -0500

    Realizando pruebas para poder verificar el funcionamiento del modelo y sus reglas impuestas

--- api-rest/routes/web.php
+++ api-rest/routes/web.php
@@ -1,11 +1,26 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return 'Bienvenido';
+Route::get('/prueba', function () {
+    $task = new Task();
+
+    $data = [
+        'body' => 'Prueba de tarea 3',
+        'user_id' => 1
+    ];
+
+    /*$task->body = $data['body'];
+    $task->user_id = $data['user_id'];
+
+    $task->save();*/
+
+    $task = Task::create($data);
+
+    return $task;
 });
