f197e634c86b1cd836b3715e5531a562c245ea93
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-13 20:40:44 -0500

    Determinando la cantidad de registros deseamos hacer para las tareas e ingresando el id del usuario se desea registrar cada tarea

--- api-rest/database/seeders/DatabaseSeeder.php
+++ api-rest/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Bryan LCL',
-            'email' => 'blcl@gmail.com',
+        $user = User::factory()->create([
+            'name' => 'Lennin BCL',
+            'email' => 'lbcl@gmail.com',
+        ]);
+
+        Task::factory(100)->create([
+            'user_id' => $user->id,
         ]);
     }
 }
