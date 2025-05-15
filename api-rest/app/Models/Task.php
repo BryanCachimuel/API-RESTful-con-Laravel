0fb7f43aae3bc482c42afb83015d1754c5d3fcc4
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-14 22:44:49 -0500

    Aplicando la relación con la entidad usuarios

--- api-rest/app/Models/Task.php
+++ api-rest/app/Models/Task.php
@@ -29,4 +29,10 @@ class Task extends Model
     /*protected $guarded = [
         ''
     ];*/
+
+    // aplicando la relación con tareas
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
