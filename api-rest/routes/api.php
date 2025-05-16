b30d48e79a3fa54c8744b2386b83b1c00cb54339
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-15 22:19:00 -0500

    Agregando las rutas para las congiguraciones realizadas de la librería jwt

--- api-rest/routes/api.php
+++ api-rest/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
     Para acceder a las rutas de la api se debe poner en la url lo siguiente
     http://127.0.0.1:8000/api
 */
+
+// rutas de jwt
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/logout', [AuthController::class, 'logout']);
+Route::post('auth/refresh', [AuthController::class, 'refresh']);
+Route::post('auth/me', [AuthController::class, 'me']);
 
 Route::get('/', function(){
     return response()->json([
