0c77c04dad138bbd5c93e152251ba78ed7ede5a9
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-16 22:33:44 -0500

    Creando una ruta para registrar un usuario de acuerdo a sus atributos

--- api-rest/routes/api.php
+++ api-rest/routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // rutas de jwt
+Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('auth/logout', [AuthController::class, 'logout']);
 Route::post('auth/refresh', [AuthController::class, 'refresh']);
