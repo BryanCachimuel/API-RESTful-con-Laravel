f333f957b2d3b9cec21101907b0cbe88690c61cd
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-22 22:55:49 -0500

    Creando un seeder para los roles

---
+++ api-rest/database/seeders/RoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = Role::create([
+            'name' => 'admin',
+            'guard_name' => 'api',
+        ]);
+
+        $role->syncPermissions([
+             'permissions.index',
+            'permissions.store',
+            'permissions.show',
+            'permissions.update',
+            'permissions.destroy',
+            'roles.index',
+            'roles.store',
+            'roles.show',
+            'roles.update',
+            'roles.destroy'
+        ]);
+    }
+}
