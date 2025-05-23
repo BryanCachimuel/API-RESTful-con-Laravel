cb3fbebcc27e4421c371d623066fa8ee5af2bc68
BryanCachimuel <bryanloyo56@gmail.com>
2025-05-22 21:56:54 -0500

    Dando funcionalidad a las funciones de index y store de los roles

---
+++ api-rest/app/Http/Controllers/Api/RoleController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RoleResource;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::all();
+        return RoleResource::collection($roles);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:roles,name',
+        ]);
+
+        $role = Role::create([
+            'name' => $request->name,
+            'guard_name' => 'api',
+        ]);
+
+        return RoleResource::make($role);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Role $role)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Role $role)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Role $role)
+    {
+        //
+    }
+}
